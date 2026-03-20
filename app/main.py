04799f379e84b343bacc36e799a64a8db3ddcde3
Harsh Aggarwal <aggharsh2005@gmail.com>
2026-03-21 03:05:07 +0530

    feat: add admin panel with user management and system stats

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.database.db import Base, engine
-from app.routes import auth, images, emotion
+from app.routes import auth, images, emotion ,admin 
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
@@ -16,6 +16,7 @@ app = FastAPI(
 app.include_router(auth.router)
 app.include_router(images.router)
 app.include_router(emotion.router)
+app.include_router(admin.router)
 
 class RootResponse(BaseModel):
     message: str
